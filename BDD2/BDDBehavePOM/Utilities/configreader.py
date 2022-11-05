8e353aeefe9717b406567a31cb3a3d27ec1fb3ec
Rudresh,Tiwari <rudreshtiwari@gmail.com>
2022-11-05 23:04:00 +0530

    BDD22

--- BDD2/BDDBehavePOM/Utilities/configreader.py
+++
@@ -1,7 +0,0 @@
-from configparser import ConfigParser
-
-
-def ConfigReader(section, Key):
-    config = ConfigParser()
-    config.read("C:\BDD2\BDDBehavePOM\ConfigurationData\config.ini")
-    return config.get(section, Key)
